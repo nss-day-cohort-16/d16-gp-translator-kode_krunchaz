dedde15837813bda477f97d3cf69b09770a486d2
Shawn M <forgeforza@outlook.com>
2016-10-24 15:47:52 -0500

    intial commit

---
+++ js/italian.js
@@ -0,0 +1,27 @@
+var LinguaFranca = (function (oldLinguaFrance) {
+	
+	var italianWords = {
+			
+			"my" : "mi",
+			"name" : "chiamo",
+			"is" : "e", 
+			"Rod" : "Rod",
+			"and" : "e",
+			"I" 	: "mi", 	
+			"like to party": "piace festaggiare" 
+
+	}; 
+
+		oldLinguaFranca.translateToItalian : function (phrase){ 
+
+			return italianWords[phrase]; 
+		};
+
+		return LinguaFranca
+
+}) (LinguaFranca) ;
+
+
+console.log(LinguaFranca); 
+
+//suona come un caso di lunedì
