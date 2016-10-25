7cc58a4ed249f3baf221da7cf4254db174bd061c
Paul Martin <Dmart331@gmail.com>
2016-10-25 09:51:40 -0500

    fixed git stuff

--- js/spanish.js
+++ js/spanish.js
@@ -1,22 +1,28 @@
 var LinguaFranca = (function(oldLinguaFranca){
-	var spanishTrans = {
-		"My": "Mi",
-		"Name": "nombre",
-		"Is": "es",
-		"Rod": "Rod",
-		"and": "y",
-		"I": "yo",
-		"like": "gusta",
-		"to":"to",
-		"party": "feista"
-	}
-	
-	oldLinguaFranca.translateToSpanish = function(input){
-		return spanishTrans[input];
-	}
 
-return oldLinguaFranca;
-	
-	
+var spanishTrans = { "My": "Mi", "name" : "nombre", "is" : "es", "Rod": "Rod", "and" : "y",  "I": "yo", "like": "gusta", "to": "to", "party": "feista"};
+		oldLinguaFranca.translateToSpanish = function(phrase) {
+
+			var arrPhrase = phrase.split(" ");
+
+			var strPhrase = "";
+			
+			for (var i=0;i < arrPhrase.length; i++){
+
+				if (i == 0) {
+
+					strPhrase = spanishTrans[arrPhrase[i]];
+					
+			} else {
+					strPhrase += " " + spanishTrans[arrPhrase[i]];
+
+			}
+
+		}
+		return strPhrase;
+
+	}
+	return oldLinguaFranca;
 
 })(LinguaFranca);
+
