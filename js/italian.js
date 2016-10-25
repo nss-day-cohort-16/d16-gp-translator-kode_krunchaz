45242a01b45ee0057c97c322de4ac664f02ba20d
Shawn M <forgeforza@outlook.com>
2016-10-25 09:37:03 -0500

    added function execution

--- js/italian.js
+++ js/italian.js
@@ -1,27 +1,45 @@
-var LinguaFranca = (function (oldLinguaFrance) {
-	
-	var italianWords = {
-			
-			"my" : "mi",
-			"name" : "chiamo",
-			"is" : "e", 
-			"Rod" : "Rod",
-			"and" : "e",
-			"I" 	: "mi", 	
-			"like to party": "piace festaggiare" 
 
-	}; 
+var LinguaFranca = (function (oldLinguaFranca) {
+    
+    var italianWords = {
+            
+            "my" : "mi",
+            "name" : "chiamo",
+            "is" : "e", 
+            "Rod" : "Rod",
+            "and" : "e",
+            "I" : "mi",     
+            "like to party": "piace festaggiare" 
 
-		oldLinguaFranca.translateToItalian = function (phrase) { 
+    }; 
 
-			return italianWords[phrase]; 
-		};
+        oldLinguaFranca.translateToItalian = function (phrase) { 
 
-		return LinguaFranca
+            var arrPhrase = phrase.split(" ");
+            var strPhrase = "";
 
-}) (LinguaFranca) ;
+            //console.log("phrase:", phrase);
+            //console.log("arrPhrase", arrPhrase.length);
+
+            for (var i=0;i < arrPhrase.length; i++){
+                //console.log("i", i);
+                if (i == 0) {
+                    strPhrase = italianWords[arrPhrase[i]];
+            } else {
+                    strPhrase += " " + italianWords[arrPhrase[i]];
+
+            }
+
+            //console.log("strPhrase", strPhrase);
+            }
+        
+        return strPhrase;  
 
 
-console.log(LinguaFranca); 
+};
 
-//suona come un caso di lunedì
+        return LinguaFranca
+
+}) (LinguaFranca);
+
+//console.log(LinguaFranca); 
